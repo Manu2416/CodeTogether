72ef91aa101e958fc9a2e4d58feaeee9d109dc0e
manu <manuelgm6@hotmail.com>
2025-02-07 11:48:20 +0100

    Cambios hechisimos

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,72 @@
 </script>
 
 <template>
+  <header>
+    <nav>
+      <div class="logo">
+        <img src="@/assets/fotos/logo_morado.png" alt="Logo Morado">
+        <h2>Innova Studio</h2>
+      </div>
+      <div class="nav_derecha">
+        <a href=""> Home </a>
+        <a href="">Portafolio</a>
+        <a href="">Rest</a>
+      </div>
+    </nav>
+</header>
+  
+  <main>
+    <section class="contenedor">
+      <article class="titulo">
+        <p class="t_1">Welcome to</p>
+        <p class="t_2">Innova.</p>
+      </article>
 
+      <article class="capa_logo">
+
+        <img src="@/assets/fotos/logo_morado.png" alt="Logo Morado">
+      </article>
+   </section>
+  </main>
+
+  <!-- <p>Graphic design studio</p>
+  <div>Aquí lo que se mueve</div>
+  <div>Aquí ellos</div>
+  <div>Contactos y branding</div> -->
+
+  <footer>
+    © 2035 by Business Name. Built on Wix Studio
+  </footer>
 </template>
 
 <style>
 /* Reset básico */
-body {
-  /* background-color: white; */
+
+
+html, body {
   margin: 0;
   padding: 0;
   font-family: Arial, sans-serif;
-  padding-top: 100px;
+  height: 100%;
 }
 
+body {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+header{
+  width: 100%;
+}
 /* Estilos del navbar */
 nav {
-  width: 100%;
   display: flex;
   justify-content: space-between;
-  align-items: center;
   padding: 20px 30px;
   background-color: white;
   color: black;
-  position: fixed;
   border-bottom: 1px solid black;
-  top: 0;
-  left: 0;
+ 
 }
 
 /* Logo y nombre */
@@ -53,32 +93,51 @@ nav {
   justify-content: center;
   align-items: center;
   gap: 10px;
+  color: white;
 
 
 }
-
-.contenedor {
-  min-height: 300px;
-  position: fixed;
-  top: 90px;
-  left: 5px;
-  margin-left: 100px;
-
+.nav_derecha:hover{
+ color: red;
+}
+main{
   width: 100%;
-  margin-bottom: 50px;
+  display: flex;
 }
 
-.titulo {
-  position: absolute;
+.contenedor{
+  height: 600px;
+  width: 100%;
+  display: flex;
+  background-color: rgba(0, 0, 255, 0.567);
+}
+.titulo{
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
   color: black;
-  background-color: rgba(255, 0, 0, 0.258);
-  font-size: 11vh;
+  width: 100%;
+
   font-family: Arial, sans-serif;
-  font-stretch: expanded;
   font-weight: bold;
   z-index: 20;
-  width: 100%;
+  padding: 20px;
+  
+  
 }
+
+.t_2{
+  border: solid rgb(219, 33, 33) 1px;
+  margin-top: -150px;
+  font-size: 25vh;
+  
+}
+.t_1{
+  margin-top: -1px;
+  border: solid rgb(219, 33, 33) 1px;
+  font-size: 25vh;
+}
+
 
 .capa_logo {
   position: absolute;
@@ -89,25 +148,34 @@ nav {
 }
 
 .capa_logo img {
-  position: fixed;
   width: 840px;
   top: 190px;
   left: -90px;
 }
-
-
 .t_1 {
-  animation: apareceTitulo 2s cubic-bezier(0.25, 0.1, 0.25, 1);
+  animation: apareceTitulo 2s ease forwards;
   /* z-index: 20; */
 
 }
 
 .t_2 {
-  animation: apareceTitulo 2s cubic-bezier(0.4, 0.1, 0.25, 1);
+  animation: apareceTitulo 2.5s ease forwards;
   /* z-index: 20; */
 
 }
 
+
+footer {
+  width: 100%;
+  height: 100px;
+  background-color: black;
+  color: white;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: auto;
+}
 
 
 
@@ -117,7 +185,7 @@ nav {
   }
 
   100% {
-    transform: translateX(0);
+    transform: translateX(10%);
   }
 }
 
